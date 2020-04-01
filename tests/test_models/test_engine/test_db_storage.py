534394bab50df304c4c523b92755a284422df3f2
Bard-Budist <op7puro@live.com>
2020-04-01 13:27:04 -0500

    Feature: Add file for unittest

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import unittest
+import MySQLdb
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import os
+
+
+class TestDbStorage(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.db = MySQLdb.connect(
+            user=os.getenv('HBNB_MYSQL_USER'),
+            passwd=os.getenv('HBNB_MYSQL_PWD'),
+            db=os.getenv('HBNB_MYSQL_DB'),
+            host=os.getenv('HBNB_MYSQL_HOST')
+        cls.cur = db.cursor()
+
+    def test_query_all(self):
+        expec = self.cur.execute("SELECT COUNT(id) FROM states ORDER BY id ASC")
+        self.assertEqual(1, expec)
