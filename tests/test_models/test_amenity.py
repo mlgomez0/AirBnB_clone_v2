4af443a5ff156f533193578c895f9a47e687fb86
mlgomez0 <mlgomez230@hotmail.com>
2020-04-03 02:41:22 +0000

    Update Review and Amenity test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,31 +1,56 @@
 #!/usr/bin/python3
-"""Test Amenity"""
+"""test for amenity"""
 import unittest
-import pep8
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
+import os
 from models.amenity import Amenity
-from models.state import State
-from models.review import Review
-
-
-class Testamenity(unittest.TestCase):
-    """ This is a unittest for class amenity"""
-
-    def test_pep8_conformance_amenity(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/amenity.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-        def test_class(self):
-            """ creates instance of amenity"""
-            amenity1 = Amenity()
-            self.assertEqual(amenity1.__class__.__name__, "Amenity")
-
-        def test_father(self):
-            """ tests is subclass"""
-            amenity1 = Amenity()
-            self.assertTrue(issubclass(amenity1.__class__, BaseModel))
+from models.base_model import BaseModel
+import pep8
+class TestAmenity(unittest.TestCase):
+   """this will test the Amenity class"""
+   @classmethod
+   def setUpClass(cls):
+       """set up for test"""
+       cls.amenity = Amenity()
+       cls.amenity.name = "Breakfast"
+   @classmethod
+   def teardown(cls):
+       """at the end of the test this will tear it down"""
+       del cls.amenity
+   def tearDown(self):
+       """teardown"""
+       try:
+           os.remove("file.json")
+       except Exception:
+           pass
+   def test_pep8_Amenity(self):
+       """Tests pep8 style"""
+       style = pep8.StyleGuide(quiet=True)
+       p = style.check_files(['models/amenity.py'])
+       self.assertEqual(p.total_errors, 0, "fix pep8")
+   def test_checking_for_docstring_Amenity(self):
+       """checking for docstrings"""
+       self.assertIsNotNone(Amenity.__doc__)
+   def test_attributes_Amenity(self):
+       """chekcing if amenity have attibutes"""
+       self.assertTrue('id' in self.amenity.__dict__)
+       self.assertTrue('created_at' in self.amenity.__dict__)
+       self.assertTrue('updated_at' in self.amenity.__dict__)
+       self.assertTrue('name' in self.amenity.__dict__)
+   def test_is_subclass_Amenity(self):
+       """test if Amenity is subclass of Basemodel"""
+       self.assertTrue(issubclass(self.amenity.__class__, BaseModel), True)
+   def test_attribute_types_Amenity(self):
+       """test attribute type for Amenity"""
+       self.assertEqual(type(self.amenity.name), str)
+   @unittest.skipIf(
+       os.getenv('HBNB_TYPE_STORAGE') == 'db',
+       "This test only work in Filestorage")
+   def test_save_Amenity(self):
+       """test if the save works"""
+       self.amenity.save()
+       self.assertNotEqual(self.amenity.created_at, self.amenity.updated_at)
+   def test_to_dict_Amenity(self):
+       """test if dictionary works"""
+       self.assertEqual('to_dict' in dir(self.amenity), True)
+if __name__ == "__main__":
+   unittest.main()
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,32 +1,62 @@
 #!/usr/bin/python3
-"""Test Review"""
+"""test for review"""
 import unittest
-import pep8
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.state import State
+import os
 from models.review import Review
-
-
-class Testreview(unittest.TestCase):
-    """ this is a testclass """
-
-    def test_pep8_conformance_review(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/review.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-
-def test_class(self):
-    """ creates a review instance"""
-    rev1 = Review()
-    self.assertEqual(rev1.__class__.__name__, "Review")
-
-    def test_father(self):
-        """ tests with basemodel """
-        rev1 = Review()
-        self.assertTrue(issubclass(rev1.__class__, BaseModel))
+from models.base_model import BaseModel
+import pep8
+class TestReview(unittest.TestCase):
+   """this will test the place class"""
+   @classmethod
+   def setUpClass(cls):
+       """set up for test"""
+       cls.rev = Review()
+       cls.rev.place_id = "4321-dcba"
+       cls.rev.user_id = "123-bca"
+       cls.rev.text = "The srongest in the Galaxy"
+   @classmethod
+   def teardown(cls):
+       """at the end of the test this will tear it down"""
+       del cls.rev
+   def tearDown(self):
+       """teardown"""
+       try:
+           os.remove("file.json")
+       except Exception:
+           pass
+   def test_pep8_Review(self):
+       """Tests pep8 style"""
+       style = pep8.StyleGuide(quiet=True)
+       p = style.check_files(['models/review.py'])
+       self.assertEqual(p.total_errors, 0, "fix pep8")
+   def test_checking_for_docstring_Review(self):
+       """checking for docstrings"""
+       self.assertIsNotNone(Review.__doc__)
+   def test_attributes_review(self):
+       """chekcing if review have attributes"""
+       self.assertTrue('id' in self.rev.__dict__)
+       self.assertTrue('created_at' in self.rev.__dict__)
+       self.assertTrue('updated_at' in self.rev.__dict__)
+       self.assertTrue('place_id' in self.rev.__dict__)
+       self.assertTrue('text' in self.rev.__dict__)
+       self.assertTrue('user_id' in self.rev.__dict__)
+   def test_is_subclass_Review(self):
+       """test if review is subclass of BaseModel"""
+       self.assertTrue(issubclass(self.rev.__class__, BaseModel), True)
+   def test_attribute_types_Review(self):
+       """test attribute type for Review"""
+       self.assertEqual(type(self.rev.text), str)
+       self.assertEqual(type(self.rev.place_id), str)
+       self.assertEqual(type(self.rev.user_id), str)
+   @unittest.skipIf(
+       os.getenv('HBNB_TYPE_STORAGE') == 'db',
+       "This test only work in Filestorage")
+   def test_save_Review(self):
+       """test if the save works"""
+       self.rev.save()
+       self.assertNotEqual(self.rev.created_at, self.rev.updated_at)
+   def test_to_dict_Review(self):
+       """test if dictionary works"""
+       self.assertEqual('to_dict' in dir(self.rev), True)
+if __name__ == "__main__":
+   unittest.main()
