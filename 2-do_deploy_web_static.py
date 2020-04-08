cace20c20504ddcf069b887bcb1becc09987aec9
mlgomez0 <mlgomez230@hotmail.com>
2020-04-08 04:14:41 +0000

    2-fix return

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,18 +11,19 @@ def do_deploy(archive_path):
 
     if os.path.isfile(archive_path):
         pre_path = archive_path.split("/")[1]
-        deply_file = put(archive_path, "/tmp/")
+        op1 = put(archive_path, "/tmp/")
         path_l = "/tmp/" + pre_path
         path_r = "/data/web_static/releases/" + pre_path.split(".")[0]
-        sudo("mkdir -p {:s}".format(path_r))
-        sudo("tar -xzf {:s} -C {:s}".format(path_l, path_r))
-        sudo("rm {:s}".format(path_l))
+        op2 = sudo("mkdir -p {:s}".format(path_r))
+        op3 = sudo("tar -xzf {:s} -C {:s}".format(path_l, path_r))
+        op4 = sudo("rm {:s}".format(path_l))
         path_m = path_r + "/web_static/*"
         path_d = path_r + "/web_static/"
-        sudo("mv {:s} {:s}".format(path_m, path_r))
-        sudo("rm -rf {:s}".format(path_d))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -s {:s} /data/web_static/current".format(path_r))
-        return True
+        op5 = sudo("mv {:s} {:s}".format(path_m, path_r))
+        op6 = sudo("rm -rf {:s}".format(path_d))
+        op7 = sudo("rm -rf /data/web_static/current")
+        op8 = sudo("ln -s {:s} /data/web_static/current".format(path_r))
+        ops = [op1, op2, op3, op4, op5, op6, op7, op8]
+        return all([op.succeeded for op in ops])
     else:
         return False
