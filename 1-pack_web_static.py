e847fac790e23b2f905b81d671b31c19fcfe6c07
mlgomez0 <mlgomez230@hotmail.com>
2020-04-09 18:45:02 +0000

    1-correct imports

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """fab generates a .tgz archive from the contents of the web_static"""
-from fabric.api import local, runs_once
+from fabric.api import local
+from fabric.decorators import runs_once
 from datetime import datetime
-import os
+from os.path import getsize
 
 
 @runs_once
 def do_pack():
     local("mkdir -p versions")
-    path = "versions/web_static_{:s}.tgz".format(
+    path_l = "versions/web_static_{:s}.tgz".format(
         datetime.now().strftime("%Y%m%d%H%M%S"))
-    print("Packing web_static to {:s}".format(path))
-    create_file = local("tar -cvzf {:s} web_static".format(path))
+    create_file = local("tar -cvzf {:s} web_static".format(path_l))
     if create_file.succeeded:
         print("web_static packed: {:s} -> {}Bytes".format(
-            path, os.path.getsize(path)))
-        return(path)
+            path_l, getsize(path_l)))
+        return(path_l)
     else:
         return None
