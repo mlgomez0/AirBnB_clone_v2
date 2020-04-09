2af254f6cbead5856c1453b1ed6414d1c51914c9
mlgomez0 <mlgomez230@hotmail.com>
2020-04-09 18:07:20 +0000

    1- correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#install Nginx
+#install Nginx and set up deply to web server
 sudo apt-get update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/
@@ -10,6 +10,6 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 echo "Hola esta es mi pagina web" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
-sudo sed -i "s@server_name localhost;@server_name _;@g" /etc/nginx/sites-enabled/default
-sudo sed -i "45 a \ \tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\n\t}" /etc/nginx/sites-enabled/default
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "45 a \ \n\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
