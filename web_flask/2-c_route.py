990ea8b7c25b2bb8c776ae0cb4659ebab51d2f9b
mlgomez0 <mlgomez230@hotmail.com>
2020-04-21 18:08:57 +0000

    fixing pep8

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,18 +4,22 @@
 
 from flask import Flask
 app = Flask(__name__)
-@app.route("/", strict_slashes = False)
+
+
+@app.route("/", strict_slashes=False)
 def home():
     return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes = False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hbpage():
     return "HBNB"
 
-@app.route("/c/<text>", strict_slashes = False)
+
+@app.route("/c/<text>", strict_slashes=False)
 def varpage(text):
     str_show = str(text).replace("_", " ")
     return "C " + str_show
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
